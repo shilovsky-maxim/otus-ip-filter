e441b938b975bb1fac2311b2f183527d78b484e2
shilovsky-maxim <max-in-3d@ngs.ru>
2021-10-08 00:04:19 +0700

    Add first implementation
    Adds first implementation of the ip filter, which implements required
    functionality.

--- ip_filter.cpp
+++ ip_filter.cpp
@@ -3,6 +3,10 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <algorithm>
+
+using VectorOfStrings = std::vector<std::string>;
+using VectorOfVectorOfStrings = std::vector<VectorOfStrings>;
 
 // ("",  '.') -> [""]
 // ("11", '.') -> ["11"]
@@ -10,9 +14,9 @@
 // ("11.", '.') -> ["11", ""]
 // (".11", '.') -> ["", "11"]
 // ("11.22", '.') -> ["11", "22"]
-std::vector<std::string> split(const std::string &str, char d)
+VectorOfStrings split(const std::string &str, char d)
 {
-    std::vector<std::string> r;
+    VectorOfStrings r;
 
     std::string::size_type start = 0;
     std::string::size_type stop = str.find_first_of(d);
@@ -29,96 +33,117 @@ std::vector<std::string> split(const std::string &str, char d)
     return r;
 }
 
+std::pair<int, bool> convert_to_int(const std::string& str)
+{
+    try
+    {
+       return std::make_pair(std::stoi(str), true);
+    }
+    catch(const std::invalid_argument&)
+    {
+    }
+    catch (const std::out_of_range&)
+    {
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+    return std::make_pair(-1, false);   
+}
+
+auto compare_strings(const std::string& first, const std::string& second)
+{
+    // If both strings can be converted to numbers, compare them as numbers.
+    // Otherwise, compare them as strings
+    auto num1 = convert_to_int(first);
+    auto num2 = convert_to_int(second);
+    if (num1.second && num2.second)
+        return num1.first > num2.first;
+    else
+        return first > second;
+}
+
+void output_ip_address(const VectorOfStrings& ip)
+{
+    bool is_first =  true;
+    for(const auto& ip_part : ip)
+    {
+        if (!is_first)
+        {
+            std::cout << ".";
+        }
+        is_first = false;
+        std::cout << ip_part;
+    }
+    std::cout << std::endl;
+}
+
+void print_ip_pool(const VectorOfVectorOfStrings& ip_pool)
+{
+    for(const auto& ip : ip_pool)
+    {
+        output_ip_address(ip);
+    }
+}
+
+/* [[maybe_unused]] attribute is used to make the code more C++14. 
+   Alternatively, parameter names could be omitted. */
 int main([[maybe_unused]]int argc, [[maybe_unused]]char const *argv[])
 {
     try
     {
-        std::vector<std::vector<std::string> > ip_pool;
+        std::vector<VectorOfStrings> ip_pool;
 
         for(std::string line; std::getline(std::cin, line);)
         {
-            std::vector<std::string> v = split(line, '\t');
-            ip_pool.push_back(split(v.at(0), '.'));
+            auto v = split(line, '\t');
+            ip_pool.push_back(split(v.front(), '.'));
         }
 
-        // TODO reverse lexicographically sort
-
-        for(std::vector<std::vector<std::string> >::const_iterator ip = ip_pool.cbegin(); ip != ip_pool.cend(); ++ip)
+        auto comparator = [](const auto& first, const auto& second)
         {
-            for(std::vector<std::string>::const_iterator ip_part = ip->cbegin(); ip_part != ip->cend(); ++ip_part)
+            return std::lexicographical_compare(first.begin(), first.end(), second.begin(), second.end(), 
+                                                [](const auto& f, const auto& s)
+                                                {
+                                                    return compare_strings(f, s);
+                                                });
+        };
+        std::sort(ip_pool.begin(), ip_pool.end(), comparator);
+
+        // It captures ip_pool by copy to be able to perform destructive operations on it
+        auto filter = [](VectorOfVectorOfStrings ip_pool, int first_byte, int second_byte = -1)
+        {
+            // We need to remove unsuitable ip addresses, so the condition is negated
+            auto filter_condition = [first_byte, second_byte](const auto& ip)
             {
-                if (ip_part != ip->cbegin())
+                return (convert_to_int(ip.at(0)).first != first_byte) || 
+                       (second_byte >= 0 && convert_to_int(ip.at(1)).first != second_byte);
+            };
+            // Not really effective but uses std algorithms by max
+            ip_pool.erase(std::remove_if(ip_pool.begin(), ip_pool.end(), filter_condition), ip_pool.end());
+            return ip_pool;
+        };
+
+        auto filter_any = [](VectorOfVectorOfStrings ip_pool, int value) mutable
+        {
+             // We need to remove unsuitable ip addresses, so the condition is negated
+            auto filter_condition = [value](const auto& ip)
+            {
+                return std::all_of(ip.begin(), ip.end(), [value](const auto& ip_part)
                 {
-                    std::cout << ".";
+                    return convert_to_int(ip_part).first != value;
+                });
+            };
+            // Not really effective but uses std algorithms by max
+            ip_pool.erase(std::remove_if(ip_pool.begin(), ip_pool.end(), filter_condition), ip_pool.end());
+            return ip_pool;   
+        };
 
-                }
-                std::cout << *ip_part;
-            }
-            std::cout << std::endl;
-        }
-
-        // 222.173.235.246
-        // 222.130.177.64
-        // 222.82.198.61
-        // ...
-        // 1.70.44.170
-        // 1.29.168.152
-        // 1.1.234.8
-
-        // TODO filter by first byte and output
-        // ip = filter(1)
-
-        // 1.231.69.33
-        // 1.87.203.225
-        // 1.70.44.170
-        // 1.29.168.152
-        // 1.1.234.8
-
-        // TODO filter by first and second bytes and output
-        // ip = filter(46, 70)
-
-        // 46.70.225.39
-        // 46.70.147.26
-        // 46.70.113.73
-        // 46.70.29.76
-
-        // TODO filter by any byte and output
-        // ip = filter_any(46)
-
-        // 186.204.34.46
-        // 186.46.222.194
-        // 185.46.87.231
-        // 185.46.86.132
-        // 185.46.86.131
-        // 185.46.86.131
-        // 185.46.86.22
-        // 185.46.85.204
-        // 185.46.85.78
-        // 68.46.218.208
-        // 46.251.197.23
-        // 46.223.254.56
-        // 46.223.254.56
-        // 46.182.19.219
-        // 46.161.63.66
-        // 46.161.61.51
-        // 46.161.60.92
-        // 46.161.60.35
-        // 46.161.58.202
-        // 46.161.56.241
-        // 46.161.56.203
-        // 46.161.56.174
-        // 46.161.56.106
-        // 46.161.56.106
-        // 46.101.163.119
-        // 46.101.127.145
-        // 46.70.225.39
-        // 46.70.147.26
-        // 46.70.113.73
-        // 46.70.29.76
-        // 46.55.46.98
-        // 46.49.43.85
-        // 39.46.86.85
-        // 5.189.203.46
+        print_ip_pool(ip_pool);
+        print_ip_pool(filter(ip_pool, 1));
+        print_ip_pool(filter(ip_pool, 46, 70));
+        print_ip_pool(filter_any(ip_pool, 46));
     }
     catch(const std::exception &e)
     {
